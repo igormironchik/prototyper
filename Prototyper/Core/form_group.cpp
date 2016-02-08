e4df154f7f595ef2dc47bcf7d581c54d50f10d13
Igor Mironchik <igor.mironchik@gmail.com>
2016-02-08 13:07:27 +0300

    Added additional move handles to group.

--- Prototyper/Core/form_group.cpp
+++ Prototyper/Core/form_group.cpp
@@ -41,7 +41,11 @@ class FormGroupPrivate {
 public:
 	FormGroupPrivate( FormGroup * parent )
 		:	q( parent )
-		,	m_handle( 0 )
+		,	m_center( 0 )
+		,	m_topLeft( 0 )
+		,	m_topRight( 0 )
+		,	m_bottomRight( 0 )
+		,	m_bottomLeft( 0 )
 	{
 	}
 
@@ -50,17 +54,45 @@ public:
 
 	//! Parent.
 	FormGroup * q;
-	//! Handles.
-	FormMoveHandle * m_handle;
+	//! Center handle.
+	FormMoveHandle * m_center;
+	//! Top-left handle.
+	FormMoveHandle * m_topLeft;
+	//! Top-right handle.
+	FormMoveHandle * m_topRight;
+	//! Bottom-right handle.
+	FormMoveHandle * m_bottomRight;
+	//! Bottom-left handle.
+	FormMoveHandle * m_bottomLeft;
 }; // class FormGroupPrivate
 
 void
 FormGroupPrivate::init()
 {
-	m_handle = new FormMoveHandle( 3.0, QPointF( 3.0, 3.0 ), q,
+	m_center = new FormMoveHandle( 3.0, QPointF( 3.0, 3.0 ), q,
 		q->parentItem(), Qt::SizeAllCursor );
-	m_handle->setZValue( 999 );
-	m_handle->hide();
+	m_center->setZValue( 999 );
+	m_center->hide();
+
+	m_topLeft = new FormMoveHandle( 3.0, QPointF( 6.0, 6.0 ), q,
+		q->parentItem(), Qt::SizeAllCursor );
+	m_topLeft->setZValue( 999 );
+	m_topLeft->hide();
+
+	m_topRight = new FormMoveHandle( 3.0, QPointF( 0.0, 6.0 ), q,
+		q->parentItem(), Qt::SizeAllCursor );
+	m_topRight->setZValue( 999 );
+	m_topRight->hide();
+
+	m_bottomRight = new FormMoveHandle( 3.0, QPointF( 0.0, 0.0 ), q,
+		q->parentItem(), Qt::SizeAllCursor );
+	m_bottomRight->setZValue( 999 );
+	m_bottomRight->hide();
+
+	m_bottomLeft = new FormMoveHandle( 3.0, QPointF( 6.0, 0.0 ), q,
+		q->parentItem(), Qt::SizeAllCursor );
+	m_bottomLeft->setZValue( 999 );
+	m_bottomLeft->hide();
 }
 
 
@@ -82,7 +114,8 @@ FormGroup::~FormGroup()
 QRectF
 FormGroup::boundingRect() const
 {
-	return QGraphicsItemGroup::boundingRect();
+	return QGraphicsItemGroup::boundingRect()
+		.adjusted( -6.0, -6.0, 6.0, 6.0 );
 }
 
 void
@@ -100,23 +133,65 @@ FormGroup::paint( QPainter * painter, const QStyleOptionGraphicsItem * option,
 		painter->drawRect( option->rect );
 
 		const QPointF p( option->rect.x() + option->rect.width() / 2.0 -
-				 d->m_handle->halfOfSize(),
+				 d->m_center->halfOfSize(),
 			 option->rect.y() + option->rect.height() / 2.0 -
-				 d->m_handle->halfOfSize() );
+				 d->m_center->halfOfSize() );
 
-		d->m_handle->setPos( mapToParent( p ) );
+		d->m_center->setPos( mapToParent( p ) );
 
-		d->m_handle->show();
+		d->m_center->show();
+
+		const QPointF p1( option->rect.x(),
+			 option->rect.y() );
+
+		d->m_topLeft->setPos( mapToParent( p1 ) );
+
+		d->m_topLeft->show();
+
+		const QPointF p2( option->rect.x() + option->rect.width() -
+				 d->m_topRight->halfOfSize() * 2.0,
+			 option->rect.y() );
+
+		d->m_topRight->setPos( mapToParent( p2 ) );
+
+		d->m_topRight->show();
+
+		const QPointF p3( option->rect.x() + option->rect.width() -
+				 d->m_bottomRight->halfOfSize() * 2.0,
+			 option->rect.y() + option->rect.height() -
+				 d->m_bottomRight->halfOfSize() * 2.0 );
+
+		d->m_bottomRight->setPos( mapToParent( p3 ) );
+
+		d->m_bottomRight->show();
+
+		const QPointF p4( option->rect.x(),
+			 option->rect.y() + option->rect.height() -
+				 d->m_bottomLeft->halfOfSize() * 2.0 );
+
+		d->m_bottomLeft->setPos( mapToParent( p4 ) );
+
+		d->m_bottomLeft->show();
 	}
 	else
-		d->m_handle->hide();
+	{
+		d->m_center->hide();
+		d->m_topLeft->hide();
+		d->m_topRight->hide();
+		d->m_bottomRight->hide();
+		d->m_bottomLeft->hide();
+	}
 }
 
 void
 FormGroup::handleMoved( const QPointF & delta, FormMoveHandle * handle )
 {
-	if( handle == d->m_handle )
-		moveBy( delta.x(), delta.y() );
+	if( handle == d->m_center ||
+		handle == d->m_topLeft ||
+		handle == d->m_topRight ||
+		handle == d->m_bottomRight ||
+		handle == d->m_bottomLeft )
+			moveBy( delta.x(), delta.y() );
 }
 
 } /* namespace Core */
